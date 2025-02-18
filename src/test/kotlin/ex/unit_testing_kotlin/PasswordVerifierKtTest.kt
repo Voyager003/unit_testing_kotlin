a18cf1392bc8525befabbb01a34147e22663a792
voyager003 <miraclex000@gmail.com>
2025-02-18 20:17:22 +0900

    test: 테스트 한 단계 위로 올리기

--- src/test/kotlin/ex/unit_testing_kotlin/PasswordVerifierKtTest.kt
+++ src/test/kotlin/ex/unit_testing_kotlin/PasswordVerifierKtTest.kt
@@ -6,14 +6,15 @@ import io.kotest.matchers.string.shouldContain
 class PasswordVerifierDescribeTest : DescribeSpec({
     describe("verifyPassword") {
         describe("with a failing rule") {
-            it("returns errors") {
-                val fakeRule = { _: String ->
-                    RuleResult(
-                        passed = false,
-                        reason = "fake reason"
-                    )
-                }
+            // fakeRule 함수를 한 단계 위로 올림
+            val fakeRule = { _: String ->
+                RuleResult(
+                    passed = false,
+                    reason = "fake reason"
+                )
+            }
 
+            it("returns errors") {
                 val errors = verifyPassword("any value", listOf(fakeRule))
 
                 errors[0] shouldContain "fake reason"
