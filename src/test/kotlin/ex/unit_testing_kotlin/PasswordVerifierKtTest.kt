dd620f6dd98a88dee22a172153b8a3062f389fb7
voyager003 <miraclex000@gmail.com>
2025-02-18 20:13:01 +0900

    test: it()문 추가
    추가 정보를 표현하는 중첩구조

--- src/test/kotlin/ex/unit_testing_kotlin/PasswordVerifierKtTest.kt
+++ src/test/kotlin/ex/unit_testing_kotlin/PasswordVerifierKtTest.kt
@@ -4,14 +4,18 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.string.shouldContain
 
 class PasswordVerifierDescribeTest : DescribeSpec({
-    describe("Password Verification") {
-        context("given a failing rule") {
-            val fakeRule = { input: String ->
-                RuleResult(passed = false, reason = "fake reason")
-            }
+    describe("verifyPassword") {
+        describe("with a failing rule") {
+            it("returns errors") {
+                val fakeRule = { _: String ->
+                    RuleResult(
+                        passed = false,
+                        reason = "fake reason"
+                    )
+                }
 
-            it("should include the rule's reason in the error messages") {
                 val errors = verifyPassword("any value", listOf(fakeRule))
+
                 errors[0] shouldContain "fake reason"
             }
         }
